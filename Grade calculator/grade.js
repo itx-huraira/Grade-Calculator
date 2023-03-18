f0a4346d1f43b0b6ba8bcddd3fca5906f15f97dc
Abu Huraira <127379942+itx-huraira@users.noreply.github.com>
2023-03-18 11:57:19 +0500

    Add files via upload

---
+++ Grade calculator/grade.js
@@ -0,0 +1,19 @@
+"use strict";
+let englishMarks = 60;
+let urduMarks = 60;
+let average = (englishMarks + urduMarks) / 2;
+if (average < 60) {
+    console.log("Grade is F");
+}
+else if (average < 70) {
+    console.log("Grade is D");
+}
+else if (average < 80) {
+    console.log("Grade is c");
+}
+else if (average < 90) {
+    console.log("Grade is B");
+}
+else if (average < 100) {
+    console.log("Grade is A");
+}
